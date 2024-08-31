5af9706a9d6feef9775da74f48c3ef8cb6d5b424
Luis Esparza <l.esparza.p@gmail.com>
2024-08-31 13:19:10 -0400

    initial commit

--- main.tf
+++ main.tf
@@ -1,10 +1,10 @@
 provider "google" {
-  project = "test-gcp"
-  region  = "us-central1"
+  project = var.project_id
+  region  = var.region
 }
 
 resource "google_cloudfunctions_function" "function" {
-  name        = "mi-funcion"
+  name        = "mi-funcion-${var.project_id}"
   description = "Mi Cloud Function"
   runtime     = "nodejs14"
 
@@ -13,20 +13,37 @@ resource "google_cloudfunctions_function" "function" {
 
   trigger_http = true
   entry_point  = "helloWorld"
+
+  available_memory_mb   = 128
+  source_repository     = {}
 }
 
 resource "google_storage_bucket" "bucket" {
-  name     = "bucket-function-le"
-  location = "US"  # Añadimos la ubicación requerida
+  name     = "bucket-para-mi-funcion-${var.project_id}"
+  location = var.region
+  force_destroy = true
 }
 
 resource "google_storage_bucket_object" "archive" {
   name   = "function-source.zip"
   bucket = google_storage_bucket.bucket.name
-  source = "./function-source.zip"
+  source = var.function_zip_path
 }
 
 variable "project_id" {
-  description = "test-gcp-434200"
+  description = "El ID del proyecto de GCP"
+  type        = stringç
+  default     = "test-gcp-434200"
+}
+
+variable "region" {
+  description = "La región de GCP para desplegar los recursos"
   type        = string
+  default     = "us-central1"
+}
+
+variable "function_zip_path" {
+  description = "La ruta al archivo ZIP de la función"
+  type        = string
+  default     = "./function-source.zip"
 }
---
+++ terraform.tfvars
@@ -0,0 +1,2 @@
+project_id = "test-gcp-434200"
+region     = "us-central1"
