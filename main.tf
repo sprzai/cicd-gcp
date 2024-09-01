e2beea3f63ff0b9c185fb5f70d8e834b19b3b840
Luis Esparza <l.esparza.p@gmail.com>
2024-08-31 20:52:04 -0400

    commit initial 2

--- main.tf
+++ main.tf
@@ -10,25 +10,25 @@ resource "google_storage_bucket" "func_bucket" {
 }
 
 resource "google_storage_bucket_object" "func_src" {
-  name = "index.zip"
+  name   = "index.zip"
   bucket = google_storage_bucket.func_bucket.name
   source = "function-source.zip"
 }
 
 resource "google_cloudfunctions_function" "func" {
-  name = "func"
-  runtime = "nodejs14"
-  description = "some function"
-  available_memory_mb = 128
+  name                  = "func"
+  runtime               = "nodejs14"
+  description           = "some function"
+  available_memory_mb   = 128
   source_archive_bucket = google_storage_bucket.func_bucket.name
   source_archive_object = google_storage_bucket_object.func_src.name
-  trigger_http = true # 2
-  entry_point = "helloWorld"
+  trigger_http          = true # 2
+  entry_point           = "helloWorld"
 }
 
 resource "google_cloudfunctions_function_iam_member" "pub_access" {
-  region = google_cloudfunctions_function.func.region
+  region         = google_cloudfunctions_function.func.region
   cloud_function = google_cloudfunctions_function.func.name
-  role = "roles/invokeFunctions"
-  member = "allUsers"
+  role           = "roles/invokeFunctions"
+  member         = "allUsers"
 }
